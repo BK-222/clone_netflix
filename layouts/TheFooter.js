6a00cadce3e30ba7a1f0a0dbc26ca0b55b7bc5dd
BK-222 <letsgetjscoding@gmail.com>
2022-12-05 13:24:37 +0000

    footer-str.

--- layouts/TheFooter.js
+++ layouts/TheFooter.js
@@ -4,24 +4,24 @@ const theFooter = {
 		<div class="w-2/3">\
 			<p>Questions? Call us.</p>\
 			<ul class="flex flex-wrap">\
-				<li>FAQ</li>\
-				<li>Help Center</li>\
-				<li>Account</li>\
-				<li>Media Center</li>\
-				<li>Investor Relations</li>\
-				<li>Jobs</li>\
-				<li>Redeem gift cards</li>\
-				<li>Buy gift cards</li>\
-				<li>Ways to Watch</li>\
-				<li>Terms of Use</li>\
-				<li>Privacy</li>\
-				<li>Cookie Preferences</li>\
-				<li>Corporate Information</li>\
-				<li>Contact us</li>\
-				<li>Speed Test</li>\
-				<li>Legal Guarantee</li>\
-				<li>Legal Notice</li>\
-				<li>Only on Netflix</li>\
+				<li class="w-1/4">FAQ</li>\
+				<li class="w-1/4">Help Center</li>\
+				<li class="w-1/4">Account</li>\
+				<li class="w-1/4">Media Center</li>\
+				<li class="w-1/4">Investor Relations</li>\
+				<li class="w-1/4">Jobs</li>\
+				<li class="w-1/4">Redeem gift cards</li>\
+				<li class="w-1/4">Buy gift cards</li>\
+				<li class="w-1/4">Ways to Watch</li>\
+				<li class="w-1/4">Terms of Use</li>\
+				<li class="w-1/4">Privacy</li>\
+				<li class="w-1/4">Cookie Preferences</li>\
+				<li class="w-1/4">Corporate Information</li>\
+				<li class="w-1/4">Contact us</li>\
+				<li class="w-1/4">Speed Test</li>\
+				<li class="w-1/4">Legal Guarantee</li>\
+				<li class="w-1/4">Legal Notice</li>\
+				<li class="w-1/4">Only on Netflix</li>\
 			</ul>\
 			<p>Netflix</p>\
 		</div>\
